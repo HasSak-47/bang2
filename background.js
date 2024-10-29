ab3d08f6cc75faa1e8308e064502f87cd1baf378
arch machine <lilith.alanis.255@gmail.com>
2024-10-29 11:20:05 -0600

    ok enough

--- background.js
+++ background.js
@@ -1,60 +1,66 @@
 
 /**
-	* @type {Object.<string, {desc: string, url: string, format: boolean}>}
+	* @returns {Promise<[Object.<string, {name: string, url: string, format: boolean}>]>}
 	*/
-const URLS = {
-	"ww": {
-		desc: "web whatsapp",
-		url: "https://web.whatsapp.com",
-		format: false,
-	},
-	"tlr": {
-		desc: "tumblr",
-		url: "https://tumblr.com",
-		format: false,
-	},
-	"drs":{
-		desc: "rust docs",
-		url: "https://docs.rs/{}",
-		format: true,
-	},
-};
-
-function filter(text){
-	split = text.split(' ');
-
-	let site = URLS[split[0]]
-	if(site === undefined)
-		return [];
-
-	let url = site.url
-	if(site.format){
-		url = site.url.replace('{}', split[1]);
+async function load_db(){
+	let bangs = await browser.storage.local.get('bangs')
+	if(bangs == null || bangs == undefined || Object.keys(bangs).length === 0){
+		browser.storage.local.set({bangs: [
+			{
+				shorthand: 'drs',
+				name: 'rust docs',
+				url : 'https://docs.rs/{}',
+				format: true,
+			},
+			{
+				shorthand: 'w',
+				name: 'wikipedia',
+				url : 'https://en.wikipedia.org/w/index.php?go=Go&search={}',
+				format: true,
+			},
+		]})
 	}
-	console.log(url)
-	
-	return [{
-		content: url,
-		description: url.desc,
-	}]
+	return await browser.storage.local.get('bangs');
 }
 
-// Update the suggestions whenever the input is changed.
-browser.omnibox.onInputChanged.addListener((text, addSuggestions) => {
-	addSuggestions(filter(text))
-});
+async function filter(text){ }
 
-browser.omnibox.onInputEntered.addListener((text, disposition) => {
-  let url = text;
-  switch (disposition) {
-    case "currentTab":
-      browser.tabs.update({url});
-      break;
-    case "newForegroundTab":
-      browser.tabs.create({url});
-      break;
-    case "newBackgroundTab":
-      browser.tabs.create({url, active: false});
-      break;
-  }
-});
+// Update the suggestions whenever the input is changed.
+// browser.omnibox.onInputChanged.addListener(async (text, addSuggestions) => { });
+
+browser.omnibox.onInputEntered.addListener(
+	/**
+	* @param {string} text 
+	*/
+	async (text, disposition) => {
+		let split = text.split(' ');
+		let db = (await load_db()).bangs;
+
+		// lmaoooo
+		let site = db.find(p => p.shorthand == split[0])
+		
+		if(site === undefined)
+			return [];
+
+		let url = site.url
+		let k = ''
+		for (let index = 1; index < split.length; ++index)
+			k += split[index];
+
+		if(site.format)
+			url = site.url.replace('{}', k);
+		console.log(url)
+
+		switch (disposition) {
+  	  case "currentTab":
+  	    browser.tabs.update({url});
+  	    break;
+  	  case "newForegroundTab":
+  	    browser.tabs.create({url});
+  	    break;
+  	  case "newBackgroundTab":
+  	    browser.tabs.create({url, active: false});
+  	    break;
+  	}
+	}
+);
