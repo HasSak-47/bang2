9a060751990f3bc52cffef4ad6d8d46151afcbeb
daniel-nix <ulises.alanis.255@gmail.com>
2024-10-30 19:44:17 -0600

    ui?

--- popup/script.js
+++ popup/script.js
@@ -1,1 +1,16 @@
 
+browser.tabs.executeScript({code: ''})
+	.then(() => {
+		document.addEventListener("submit", () => 
+			document.getElementById('popup-botton').addEventListener('click', () => {
+				let name = document.getElementById('input-name');
+				let shorthand = document.getElementById('input-shorthand');
+				let url = document.getElementById('input-url');
+				name.value = 'test val';
+				console.log('log!');
+		}));
+	})
+	.catch(e => {
+		console.error('kurwą mać');
+		console.error(e);
+	});
