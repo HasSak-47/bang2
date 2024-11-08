a30e5c8d89304289b7f1d48be4170a386093aae0
Ulises Alanis <ulises.alanis.255@gmail.com>
2024-11-08 09:27:32 -0600

    stuff

--- popup/script.js
+++ popup/script.js
@@ -1,26 +1,62 @@
+/**
+	* @param {SubmitEvent} event 
+	*/
+async function add_bang(event) {
+  event.preventDefault();
+	const form_data = new FormData(event.target);
+	const data = Object.fromEntries( form_data.entries() );
+
+	console.log(form_data);
+	console.log(data);
+
+	const db = await import('../utils/db.js');
+	await db.add_data(
+		data["bang-name"],
+		data["bang-shorthand"],
+		data["bang-url"]
+	);
+}
+
+/**
+	* @param {InputEvent} event 
+	* @param {string} name
+	*/
+async function search_bang(event, name) {
+
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	console.log('pop up opened');
-  let form = document.getElementById('bang-form');
-	if(form == null){
-		console.error('form not foudn')
-		return;
-	}
-	console.log(form);
 
-	form.addEventListener('submit', async function (event) {
-    event.preventDefault();
-		const form_data = new FormData(event.target);
-		const data = Object.fromEntries( form_data.entries() );
+	let option_list   = document.getElementById('option-list');
+	let option_add    = document.getElementById('option-add');
+	let option_search = document.getElementById('option-search');
 
-		console.log(form_data);
-		console.log(data);
+	let list   = document.getElementById('list-bang');
+  let form   = document.getElementById('add-bang');
+	let search = document.getElementById('search-bang');
+
+	let search_name = document.getElementById('list-name');
+	let search_bang = document.getElementById('list-bang');
+
+	option_list.addEventListener('click', async function() {
+		list.style.display   = 'block';
+		form.style.display   = 'none';
+		search.style.display = 'none';
+	});
+
+	option_add.addEventListener('click', async function() {
+		list.style.display   = 'none';
+		form.style.display   = 'block';
+		search.style.display = 'none';
+	});
+
+	option_search.addEventListener('click', async function() {
+		list.style.display   = 'none';
+		form.style.display   = 'none';
+		search.style.display = 'block';
+	});
 
 
-		const db = await import('../utils/db.js');
-		await db.add_data(
-			data["bang-name"],
-			data["bang-shorthand"],
-			data["bang-url"]
-		);
-  });
+	form.addEventListener('submit', add_bang);
 });
