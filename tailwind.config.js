431d67e6802e907668526066fc11f3ad9c18e5e0
arch machine <lilith.alanis.255@gmail.com>
2025-02-10 17:03:23 -0600

    typescript?

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
 		colors: {
-			'ccol-0': '#EEF0F2',
-			'ccol-1': '#C6C7C4',
-			'ccol-2': '#A2999E',
-			'ccol-3': '#846A6A',
-			'ccol-4': '#353B3C',
+			'ccol-0': '#eef0f2',
+			'ccol-1': '#c6c7c4',
+			'ccol-2': '#a2999e',
+			'ccol-3': '#846a6a',
+			'ccol-4': '#353b3c',
 		},
     extend: {
       width: {
